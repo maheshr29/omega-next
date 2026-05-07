71a2e4a9484d16fc5a67f5fe3868e92910ca6411
maheshr29 <118993109+maheshr29@users.noreply.github.com>
2026-05-07 19:17:42 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <></>
-    // <main className="mx-auto max-w-3xl px-6 py-16 font-sans">
-    //   <h1 className="text-3xl font-semibold tracking-tight">DwyerOmega</h1>
-    // </main>
+    <main className="mx-auto max-w-3xl px-6 py-16 font-sans">
+      <h1 className="text-3xl font-semibold tracking-tight">DwyerOmega</h1>
+    </main>
   );
 }
